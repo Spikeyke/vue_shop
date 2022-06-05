88240dd0f7dcc35eab6f940b8f7f52eab9bb272b
spiketyke <10179800+spiketyke@user.noreply.gitee.com>
2022-06-06 00:15:22 +0800

    格式化代码

---
+++ .history/.eslintrc_20220605211203.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'space-before-function-paren': 0
+  }
+}
